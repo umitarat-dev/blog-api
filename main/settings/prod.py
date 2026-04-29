0f016564fbc4408bd927e35c25178f562e97c062
Ümit Arat <umitarat8098@gmail.com>
2026-04-29 17:31:22 +0300

    fix: corsheaders fixed

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -22,3 +22,19 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Güvenlik (HTTPS/CSRF)
 CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
+
+
+# 1. Django'ya Railway proxy'sine güvenmesini söyleyin
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True # HTTP'yi otomatik HTTPS'ye yönlendirir
+
+# 2. CSRF Güvenilir Kaynaklar (Statik yazmak daha garantidir)
+# ALLOWED_HOSTS "*" olduğunda list comprehension bazen sorun çıkarabilir.
+CSRF_TRUSTED_ORIGINS = [
+    "https://blog-api-product.up.railway.app",
+    "http://blog-api-product.up.railway.app"
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://blog-api-product.up.railway.app",
+]
