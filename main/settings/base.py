43862880141e4086863fa4151653f767feeaffb9
Ümit Arat <umitarat8098@gmail.com>
2026-04-29 16:50:00 +0300

    feat: change the python version3.12 to Dockerfile

--- main/settings/base.py
+++ main/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent # Bir tık yukarı çı
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-build-stage-key-replace-me')
 
 
 # Application definition
